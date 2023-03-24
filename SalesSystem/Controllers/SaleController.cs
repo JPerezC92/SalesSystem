bb0befbf915300a0bf36a8636722f20a055e5a68
Philip Perez <jperez.c92@gmail.com>
2023-03-24 01:30:30 -0500

    added sale post endpoint

---
+++ SalesSystem/Controllers/SaleController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Models;
+using SalesSystem.Models.Request;
+using SalesSystem.Models.Response;
+
+namespace SalesSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult add(SaleReq req)
+        {
+            Response oResponse = new();
+
+            try
+            {
+                using (SalesSystemContext db = new())
+                {
+                    Sale oSale = new();
+                    oSale.Total = req.Total;
+                    oSale.Date = DateTime.Now;
+                    oSale.IdClient = req.IdClient;
+                    db.Sales.Add(oSale);
+                    db.SaveChanges();
+
+                    foreach (var detail in req.DetailList)
+                    {
+                        SaleDetail oSaleDetail = new();
+                        oSaleDetail.Quantity = detail.Quantity;                        
+                        oSaleDetail.IdProduct = detail.IdProduct;
+                        oSaleDetail.UnitPrice = detail.UnitPrice;                      
+                        oSaleDetail.Total = detail.Total;
+                        oSaleDetail.IdSale = oSale.Id;
+
+                        db.SaleDetails.Add(oSaleDetail);
+                        db.SaveChanges();
+
+                    }
+
+                    oResponse.Sucess= true;
+                }
+
+
+            }
+            catch { }
+
+            return Ok(oResponse);
+        }
+
+    }
+}
---
+++ SalesSystem/Models/Request/SaleReq.cs
@@ -0,0 +1,18 @@
+﻿namespace SalesSystem.Models.Request
+{
+    public class SaleReq
+    {
+        public int IdClient { get; set; }
+        public decimal Total { get; set; }
+        public List<Detail> DetailList { get; set; } = new List<Detail>();
+    }
+
+    public class Detail
+    {
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+        public int IdProduct { get; set; }
+
+    }
+}
